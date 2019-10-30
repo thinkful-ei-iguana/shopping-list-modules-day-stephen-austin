6216231c74863d93ac82e92511ce76af37434cef
AustinT <austintumlinson@gmail.com>
2019-10-30 14:08:05 -0500

    step 5 completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 import shoppingList from './shopping-list.js';
-
+import store from './store.js';
 const main = function () {
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
 
 $(main);
+
--- scripts/store.js
+++ scripts/store.js
@@ -1,10 +1,51 @@
-//import item from './item-js';
+import item from './item.js';
 
 const items= [];
 const hideCheckedItems= false;
 
+const findById = function(id) {
+ const item = items.find(item => item.id === id);
+ return item;
+}
+
+const addItem = function(name) {
+  try {
+    item.validateName(name);
+    const newItem = item.create(name);
+    items.push(newItem);
+  } 
+  catch {
+    console.log(`Cannot add item: ${error.message}`);
+  }
+}
+
+const findAndToggleChecked = function(id) {
+  this.findById(id).toggle(items.checked);
+}
+
+const findAndUpdateName = function(id, newName) {
+  try {
+    item.validateName(newName);
+    const item = findById(id);
+    item.name = newName;
+  }
+    catch {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+}
+
+const findAndDelete = function(id) {
+  const deleteItem = items.findIndex(item => id === item.id);
+  items.splice(deleteItem, 1);
+}
+
 
 export default {
   items,
-  hideCheckedItems: hideCheckedItems
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete
 };
