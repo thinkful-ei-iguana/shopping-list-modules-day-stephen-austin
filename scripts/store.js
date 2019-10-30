93066673dcfd233b8a874bb8311fa544e53fc5bb
AustinT <austintumlinson@gmail.com>
2019-10-30 13:02:59 -0500

    fixed checked items

--- scripts/store.js
+++ scripts/store.js
@@ -1,9 +1,10 @@
-import item from './item.js';
+//import item from './item-js';
 
 const items= [];
 const hideCheckedItems= false;
 
+
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems: hideCheckedItems
 };
